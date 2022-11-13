32f4764a682855ccd072fd1d69cd74996d289114
marcoantonioni <antonioni.marco@gmail.com>
2022-11-13 10:59:57 +0100

    added MyKogitoPerformanceDm project

---
+++ MyKogitoPerformanceDm/src/main/java/marco/demos/kogito/ConsumeCPU.java
@@ -0,0 +1,35 @@
+package marco.demos.kogito;
+
+import java.math.BigDecimal;
+
+public class ConsumeCPU {
+    public static String wasteCpu(BigDecimal loops, BigDecimal innerLoops) {
+        int _loops = loops.intValue();
+        int _innerLoops = innerLoops.intValue();
+        if (_loops < 1) _loops = 1;
+        if (_innerLoops < 1) _innerLoops = 1;
+        System.out.println("===> wasteCpu loops: "+_loops);        
+        System.out.println("===> wasteCpu innerLoops: "+_innerLoops);        
+        for (int i = 0; i < _loops; i++) {
+            for (int j = 0; j < _innerLoops; j++) {
+                Math.pow(1234.1234, 2345.2345);    
+            }            
+        }        
+        return "done";
+    }
+
+    public static String wasteCpuForTime(BigDecimal milliseconds) {
+        int _milliseconds = milliseconds.intValue();
+        if (_milliseconds < 1) _milliseconds = 1;
+        System.out.println("===> wasteCpuForTime milliseconds: "+_milliseconds);        
+        boolean stop = false;
+        long _startTime = System.currentTimeMillis();
+        do {
+            Math.pow(1234.1234, 2345.2345);    
+            long _currentTime = System.currentTimeMillis();
+            stop = (_currentTime - _startTime) >= _milliseconds;
+        } while(! stop);
+
+        return "done";
+    }
+}
