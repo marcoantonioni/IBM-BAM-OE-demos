b43e0ae7bbc0b57e6d0d6e3899e684d096542f10
marcoantonioni <antonioni.marco@gmail.com>
2022-11-14 09:20:49 +0100

    updated with quarkus-micrometer-registry-prometheus

--- MyKogitoPerformanceDm/src/main/java/marco/demos/kogito/GreetingResource.java
+++ MyKogitoPerformanceDm/src/main/java/marco/demos/kogito/GreetingResource.java
@@ -1,16 +1,23 @@
 package marco.demos.kogito;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    MeterRegistry registry;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        registry.counter("greeting_counter").increment();
+        return "Hello !!!";
     }
 }
