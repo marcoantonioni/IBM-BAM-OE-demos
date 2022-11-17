2411362fc53d6d2cbf2ad6644eef7980ebe8e3bb
marcoantonioni <antonioni.marco@gmail.com>
2022-11-17 07:28:01 +0100

    update logs

--- MyKogitoPerformanceDm/src/main/java/marco/demos/kogito/ConsumeCPU.java
+++ MyKogitoPerformanceDm/src/main/java/marco/demos/kogito/ConsumeCPU.java
@@ -3,12 +3,14 @@ package marco.demos.kogito;
 import java.math.BigDecimal;
 
 public class ConsumeCPU {
-    public static String wasteCpu(BigDecimal loops, BigDecimal innerLoops) {
+    public static String wasteCpu(BigDecimal loops, BigDecimal innerLoops) {        
         int _loops = loops.intValue();
         int _innerLoops = innerLoops.intValue();
         if (_loops < 1) _loops = 1;
         if (_innerLoops < 1) _innerLoops = 1;
+
         // System.out.println("===> wasteCpu loops["+_loops+"] innerLoops["+_innerLoops+"]");        
+
         for (int i = 0; i < _loops; i++) {
             for (int j = 0; j < _innerLoops; j++) {
                 Math.pow(1234.1234, 2345.2345);    
@@ -20,7 +22,9 @@ public class ConsumeCPU {
     public static String wasteCpuForTime(BigDecimal milliseconds) {
         int _milliseconds = milliseconds.intValue();
         if (_milliseconds < 1) _milliseconds = 1;
-        // System.out.println("===> wasteCpuForTime milliseconds: "+_milliseconds);        
+
+        System.out.println("===> wasteCpuForTime milliseconds: "+_milliseconds);        
+
         boolean stop = false;
         long _startTime = System.currentTimeMillis();
         do {
